56e882a587796ec8f8815e4141becf6f4a6716eb
devbydaylight <devbydaylight@protonmail.com>
2020-10-20 22:35:45 -0500

    adding system_mon_tools.sh script to bash repo.

---
+++ system_mon_tools.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# System diagnostics tooling user data script.
+#
+# Installs epel repository, atop, iotop, htop, iftop, nc, arpwatch.
+#
+# Script can be copied and placed into the User data field when launching an EC2 instance.
+# Note this should be run as user data just once and removed from user data once the instance
+# has been launched.
+#
+# Note: if running from command line, be sure to add execute function and add sudo before executing 
+# the bash script:
+# 
+# $ chmod +x system_mon_tool.sh
+# $ sudo ./system_mon_tool.sh
+
+amazon-linux-extras install epel
+
+yum install -y atop iotop htop iftop nmap-ncat arpwatch
