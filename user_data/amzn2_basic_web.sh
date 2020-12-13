8030d798bb24a8131d4999747128540625bb3c47
devbydaylight <devbydaylight@devbydaylights-MacBook-Pro.local>
2020-12-13 01:00:48 -0600

    moving things around and renaming user data scripts

---
+++ user_data/amzn2_basic_web.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Rudimentary bash shell bootstrap script that installs basic apache web server
+# on AWS EC2 instance running Amazon Linux 2 AMI. Can be used in user data scripts as well for testing purposes. 
+
+yum update -y 
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+cd /var/www/html
+echo "<html><body><h1>A Basic Website</h1></body></html>" > index.html
