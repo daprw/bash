491a382988492f76b58284784114e09d28045ef9
devbydaylight <50723211+devbydaylight@users.noreply.github.com>
2021-05-05 11:04:26 -0500

    Update system_mon_tools.sh

--- system_mon_tools.sh
+++ system_mon_tools.sh
@@ -2,7 +2,7 @@
 
 # System diagnostics tooling user data script.
 #
-# Installs epel repository, atop, iotop, htop, iftop, nc, arpwatch.
+# Installs epel repository, atop, iotop, htop, iftop, nc, arpwatch on Amazon Linux 2 AMI based EC2 instances.
 #
 # Script can be copied and placed into the User data field when launching an EC2 instance.
 # Note this should be run as user data just once and removed from user data once the instance
