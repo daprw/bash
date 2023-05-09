2e22ffe4ea69a3c96878cd22074881cb5002eae0
daprw <50723211+daprw@users.noreply.github.com>
2023-05-09 16:01:01 -0400

    Update logger.sh

--- logger.sh
+++ logger.sh
@@ -7,6 +7,8 @@
 # regex pattern for most names: ^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$
 # regex pattern for unicode names: ^([A-Za-z\u00C0-\u017F'\-]+)(\s[A-Za-z\u00C0-\u017F'\-]+)*(\s[A-Za-z\u00C0-\u017F'\-]+)?$
 # regex pattern for most names part 2: \b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+
+# regex pattern for most names part 3: ^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)
+# regex pattern for emails RFC 5322: ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$
 while true; 
 do 
   echo $(date) INFO: Transaction submitted by user email $(openssl rand -hex 12)@$(shuf -n1 -e gmail.com yahoo.com hotmail.com outlook.com aol.com protonmail.com icloud.com example.co.uk sample.org pinterest.co.uk ipinfo.io)
